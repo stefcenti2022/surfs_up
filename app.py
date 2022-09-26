4d555b8ffc168837f21c0c0acc6ae960b505f1ed
stefcenti2022 <110138522+stefcenti2022@users.noreply.github.com>
2022-09-25 21:11:11 -0400

    New files for SQLite module.

---
+++ app.py
@@ -0,0 +1,11 @@
+# Import Dependencies
+from flask import Flask
+
+# Create a New Flask App Instance
+app = Flask(__name__)
+
+# Create Flask Routes
+@app.route('/')
+
+def hello_world():
+    return 'Hello world'
